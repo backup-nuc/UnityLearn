49d13e12a589b851d56412de9c8be4b8ddc8cdc7
BaiRong-NUC <nucgby@proton.me>
2025-09-18 14:27:29 +0800

    初始化git仓库

---
+++ Assets/Sctipts/1. 生命周期函数/Life.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log($"{i}");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}

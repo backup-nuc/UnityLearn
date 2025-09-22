66ff952c1a97142e0eeccbba96a428a6db24a590
BaiRong-NUC <nucgby@proton.me>
2025-09-22 15:20:19 +0800

    unity Input的相关API

---
+++ Assets/Sctipts/9. Input/TestInput.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class TestInput : MonoBehaviour
+{
+    void Update()
+    {
+        // 1. 获取鼠标在屏幕的位置(游戏界面)
+        // print("鼠标在屏幕的位置" + Input.mousePosition);
+        // 2. 检测鼠标输入 0左键 1右键 2中键 (游戏界面)
+        //  鼠标按下
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("鼠标左键按下");
+        }
+        //  鼠标抬起
+        if (Input.GetMouseButtonUp(0))
+        {
+            print("鼠标左键抬起");
+        }
+        //  鼠标长按,按下,抬起都会进入(长按会一直进入)
+        if (Input.GetMouseButton(1))
+        {
+            print("鼠标右键长按,按下,抬起");
+        }
+        //中键滚动 y = -1下  1 上 0没有滚。返回值是vector的值
+        print(Input.mouseScrollDelta);
+        // 3. 检测键盘输入 
+        if (Input.GetKeyDown(KeyCode.W)) //W不区分大小写 Input.GetKeyDown("w"); //小写,区分大小写
+        {
+            print("W/w键按下");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            print("W/w键抬起");
+        }
+        //键盘长按,按下,抬起都会进入
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("W/w键长按");
+        }
+        //4. 设置默认轴输入
+        //  键盘AD按下时返回- 1到1之间的变换
+        //  键盘SW按下时返回-1到1之间的变换
+        //  鼠标横向移动时-1到1左右
+        //  鼠标竖向移动时-1到1下上
+    }
+}

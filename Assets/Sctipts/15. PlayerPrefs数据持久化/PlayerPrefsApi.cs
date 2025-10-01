dd41f0e3e02a93d0fb21c45ff315917abf54def4
GongBaiRong <gby@edu.nuc.email>
2025-10-01 10:24:08 +0800

    PlayerPrefs Unity 自带的数据持久化 API

---
+++ Assets/Sctipts/15. PlayerPrefs数据持久化/PlayerPrefsApi.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsApi : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. PlayerPrefs是Unity提供的用于存储玩家数据的公共类
+
+        // 2. PlayerPrefs可以存储三种类型的数据：整数(int)、浮点数(float)和字符串(string)
+        // 键值:string类型
+        // 值: int float string
+
+        PlayerPrefs.SetInt("HighScore", 100); // 存储整数
+        PlayerPrefs.SetFloat("Volume", 0.8f); // 存储浮点数
+        PlayerPrefs.SetString("PlayerName", "Hero"); // 存储字符串
+
+        // 直接调用Set方法存储数据,数据会存到内存中,当程序退出时,数据会自动保存到磁盘中,游戏崩溃时,数据可能会丢失
+        // 如果需要立即保存数据,可以调用PlayerPrefs.Save()方法
+        PlayerPrefs.Save();
+
+        // 注意: 
+        // PlayerPrefs有局限性,它只能存储3种类型的数据,别的类型只能减低精度,或上升精度来存储(bool类型转化为存int类型)
+        // 相同的建值会覆盖之前的数据
+        // PlayerPrefs适合存储少量数据,大量数据建议使用文件或数据库
+
+        // 3. 读取数据
+        int highScore = PlayerPrefs.GetInt("HighScore", 0); // 第二个参数是默认值,如果没有找到对应的键,则返回默认值
+        float volume = PlayerPrefs.GetFloat("Volume", 1.0f);
+        string playerName = PlayerPrefs.GetString("PlayerName", "Unknown");
+
+        print("HighScore: " + highScore);
+        print("Volume: " + volume);
+        print("PlayerName: " + playerName);
+
+        // 4. 检查是否存在某个键
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            print("HighScore键存在");
+        }
+        else
+        {
+            print("HighScore键不存在");
+        }
+
+        // 5. 删除某个键
+        PlayerPrefs.DeleteKey("HighScore");
+        if (!PlayerPrefs.HasKey("HighScore"))
+        {
+            print("HighScore键已删除");
+        }
+        // 删除所有键
+        // PlayerPrefs.DeleteAll();
+    }
+}
